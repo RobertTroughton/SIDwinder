6be8f0305e159e8d99571e0d334dec848150aeac
Robert Troughton <me@roberttroughton.co.uk>
2025-08-20 18:01:14 +0700

    Update!

--- SIDPlayers/RaistlinBars/RaistlinBars.asm
+++ SIDPlayers/RaistlinBars/RaistlinBars.asm
@@ -496,7 +496,7 @@ UpdateBarDecay: {
 	//; Target is higher - move up quickly
 	lda barHeights, x
 	clc
-	adc #4
+	adc #16
 	cmp targetBarHeights, x
 	bcc !storeHeight+
 	lda targetBarHeights, x		//; Don't overshoot
@@ -506,7 +506,7 @@ UpdateBarDecay: {
 	//; Target is lower - move down slowly
 	lda barHeights, x
 	sec
-	sbc #2
+	sbc #4
 	cmp targetBarHeights, x
 	bcs !storeHeight+
 	lda targetBarHeights, x		//; Don't undershoot
