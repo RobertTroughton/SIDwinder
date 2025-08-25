67b188e3ec2ae424612abf665dc18dd2a2174a09
Robert Troughton <me@roberttroughton.co.uk>
2025-08-25 22:09:46 +0700

    Cleaner!

--- SIDPlayers/RaistlinBarsWithLogo/RaistlinBarsWithLogo.asm
+++ SIDPlayers/RaistlinBarsWithLogo/RaistlinBarsWithLogo.asm
@@ -1011,10 +1011,12 @@ div16mul3:					.fill 128, ((3.0 * i) / 16.0)
 	.byte $aa, $54, $aa, $54, $aa, $54, $aa, $54
 
 * = BITMAP_SCR0_DATA "Screen 0"
-	.fill $400, $00
+	.fill LOGO_HEIGHT * 40, $00
+	.fill $400 - (LOGO_HEIGHT * 40), $20
 
 * = BITMAP_SCR1_DATA "Screen 1"
-	.fill $400, $00
+	.fill LOGO_HEIGHT * 40, $00
+	.fill $400 - (LOGO_HEIGHT * 40), $20
 
 * = BITMAP_MAP_DATA "Bitmap"
 	.fill $2000, $00
